6a3f799ba2884fa3cf48768aa16f0330713abe8f
SaltyStarfish <eaa25stje@students.eaaa.dk>
2025-12-05 13:46:20 +0100

    header opdateret

--- header.js
+++ header.js
@@ -1,13 +1,12 @@
 const burger = document.querySelector("#burger");
 const burger_menu = document.querySelector("nav>menu");
 
-burger.onclick = visBurgerMenu;
+burger.onclick = visBurgerMenu; 
 
 function visBurgerMenu() {
   //https://www.w3schools.com/howto/howto_js_toggle_hide_show.asp
   if (burger_menu.style.display === "none") {
     burger_menu.style.display = "block";
-
   } 
   else {
     burger_menu.style.display = "none";
@@ -15,4 +14,5 @@ function visBurgerMenu() {
 }
 
 
-onclick="myFunction(this)"
+
+
