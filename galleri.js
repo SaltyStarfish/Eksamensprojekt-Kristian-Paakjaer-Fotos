873e7cc4b6c5a30e8ce3fd9371bb90d66b490f4e
SarahNevin <eaa25sjn@students.eaaa.dk>
2025-12-09 22:20:56 +0100

    Lightbox tilføjet

--- galleri.js
+++ galleri.js
@@ -1,15 +1,37 @@
-const slider = document.querySelector(".galleri-slider");
+const slider = document.querySelector('.galleri-slider');
 
-document.querySelector(".venstre").addEventListener("click", () => {
+document.querySelector('.venstre').addEventListener('click', () => {
     slider.scrollBy({
         left: -400,
-        behavior: "smooth",
+        behavior: 'smooth',
     });
 });
 
-document.querySelector(".hoejre").addEventListener("click", () => {
+document.querySelector('.hoejre').addEventListener('click', () => {
     slider.scrollBy({
         left: 400,
-        behavior: "smooth",
+        behavior: 'smooth',
     });
 });
+
+const lightbox = document.createElement('div')
+lightbox.id = 'lightbox'
+document.body.appendChild(lightbox)
+
+const images = document.querySelectorAll('img')
+images.forEach(image => {
+    image.addEventListener('click', e => {
+        lightbox.classList.add('active')
+        const img = document.createElement('img')
+        img.src = image.src
+        while (lightbox.firstChild) {
+            lightbox.removeChild(lightbox.firstChild)
+        }
+        lightbox.appendChild(img)
+    })
+})
+
+lightbox.addEventListener('click', e => {
+    if (e.target !== e.currentTarget) return
+    lightbox.classList.remove('active')
+})
