ea27d18dea1f85308bff8911b15fe5e40b02ae4c
SaltyStarfish <eaa25stje@students.eaaa.dk>
2025-12-13 21:45:18 +0100

    forældet script fjernet

--- header.js
+++ header.js
@@ -1,8 +1,13 @@
+// Finder burger-ikonet
 const burger = document.querySelector("header #btn_burger");
-let responsive_topnav = document.querySelector("header .responsive-topnav");
 
+// Finder navigationsmenuen
+const responsive_topnav = document.querySelector("header .responsive-topnav");
+
+// Lytter efter onclick og kører funktion
 burger.onclick = toggleBurgerMenu; 
 
+//"toggle"-funktion: Tilføjer eller fjerner en class'en "aktiv", som er stylet anderledes i css, og afgør, om menuen vises eller ej, og om de hvide streger former en burger eller et kryds
 function toggleBurgerMenu() {
    responsive_topnav.classList.toggle("aktiv");
 }
--- hero.js
+++ hero.js
@@ -1,60 +1,27 @@
-/*const hero_slide1 = document.getElementById("div_hero-content-1");
-const hero_slide2 = document.getElementById("div_hero-content-2");
-const hero_slide3 = document.getElementById("div_hero-content-3");
-const hero_slide4 = document.getElementById("div_hero-content-4");
-const hero_slide5 = document.getElementById("div_hero-content-5");
-const hero_slide6 = document.getElementById("div_hero-content-6");
-
-const hero_karrusel = [hero_slide1, hero_slide2, hero_slide3, hero_slide4, hero_slide5, hero_slide6];
-
-// hero_karrusel[0].style.display = "flex";
-// hero_karrusel[1].style.display = "flex";
-// hero_karrusel[2].style.display = "none";
-// hero_karrusel[3].style.display = "none";
-// hero_karrusel[4].style.display = "none";
-// hero_karrusel[5].style.display = "none";
-
-let vistNu = 0
-
-//for-loop
-for (i=0;i<hero_karrusel.length;i++){
-hero_slide1[i].style.display = "none";}
-
-window.setTimeout((skiftBilledeOgKomIgen, 3000));
-
-function skiftBilledeOgKomIgen(){
-  hero_karrusel[vistNu].style.display = "flex";
-  vistNu++;
-  if(vistNu >= hero_karrusel.length){
-    vistNu = 0;
-  }
-};*/
-
-
-/*måske findes en bedre løsning med arrays, og ændring af innerHTML via for-loops*/
-
-let i = -1; //for at den viser slide 1 først
+//Henter alle hero-content-boxe - altså de forskellige sider, som der skal skiftes mellem - giver er HTML-collection, hvilket minder om et array
 const slides = document.querySelectorAll(".div_hero-content");
+let i = -1; //for at den viser slide 1 først
 
-// start slideshow-funktionen
+// Slideshow-funktionen skal starte af sig selv, så ingen eventlistener er nødvendig
 showSlides();
 
 //definér slideshow-funktionen:
 function showSlides() {
-  // skjul alle slides
+
+  // skjuler alle slides som udgangspunkt
   slides.forEach(slide => slide.style = "display: none");
 
-  // "forøg slide index" - aka: bliver til næste slide
+  // forøger "slide-indexet" - dvs index tallet stiger, indtil det når "længden" af vores HTML-collection. Så starter den forfra*.
   i = (i + 1) % slides.length;
-  //"%" kaldes en "modulo-operator" og er en matematisk operator, som finder "divitions-resten" . Det virker lidt random, men fungerer enormt godt her! Når (i+1) er mindre en længden af arrayet giver divisionen ikke et helt tal, og derfor vil der være en divitionsrest på det samme tal, som (i+1). Men hvis (i+1) giver præcis længden på arrayet, går divisionen op, og der er derfor en divisionsrest på 0, så i = 0, og vi hopper tilbage til start! Genialt!
 
-  // vis det nuværende slide
+
+  // vis det nuværende slide, som så skifter, fordi index-tallet skifter
   slides[i].style = "display: flex";
 
-  // skift til næste slide hver 4. sekund
+  // kører loopet igennem hver fjerde sekund - dvs. skifter til næste slide hver 4. sekund
   setTimeout(showSlides, 4000);
 }
 
+//*men hvorfor kører den automatisk i loop? Pga module-operatoren! 
 
-
-//men hvorfor kører den automatisk i loop? Pga module-operatoren!
+//"%" kaldes en "modulo-operator" og er en matematisk operator, som finder "divisions-resten" . Det virker lidt random, men fungerer enormt godt her! Når (i+1) er mindre en længden af "arrayet" (eller HTML-collectionen) giver divisionen ikke et helt tal, og derfor vil der være en divisionsrest på det samme tal, som (i+1). Men hvis (i+1) giver præcis længden på arrayet, går divisionen op, og der er derfor en divisionsrest på 0, så i = 0, og vi hopper tilbage til start! Genialt!
